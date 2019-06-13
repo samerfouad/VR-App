721f1d02284105f3410b78aafe16462fd0654c4a
Samer Fouad <samerfouad@Samers-MacBook-Air.local>
2019-06-14 01:01:33 +0200

    Third Commit

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express = require("express"),
 app = express(),
-http = require("http").Server(app).listen(3000);
+http = require("http").Server(app).listen(8080);
 app.use("/css", express.static("./css"))
 app.use("/js", express.static("./js"))
 app.use("/img",express.static("./img"))
-console.log("server is running at port 3000")
+console.log("server is running at port 8080")
 
 app.get('/', function(req,res){
     res.sendFile(__dirname + '/index.html');
