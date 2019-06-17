e7b17f264217d3fe6ccd80e2b6d185317005ee0a
BlueWhale <samerfouad@outlook.com>
2019-06-18 00:43:18 +0200

    F commit

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ http = require("http").Server(app).listen(8080);
 app.use("/css", express.static("./css"))
 app.use("/js", express.static("./js"))
 app.use("/img",express.static("./img"))
+app.use(express.static('print.html'))
 console.log("server is running at port 8080")
 
 app.get('/', function(req,res){
     res.sendFile(__dirname + '/index.html');
 })
+
+
 
 
 
