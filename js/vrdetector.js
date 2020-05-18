e9a93517f05a34b07dfd97f5b909108a3b0712fe
samerfouad <samerfouad@outlook.com>
2020-05-18 14:30:11 +0200

    Update vrdetector.js

--- js/vrdetector.js
+++ js/vrdetector.js
@@ -2,5 +2,5 @@ var VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3'
 
 var visualRecognition = new VisualRecognitionV3({
 	version: '2018-03-19',
-	iam_apikey: 'roWB-rk8mD-yrMOk2uFrrg-SftgDbYfFXyKefTwerC6i'
+	iam_apikey: ''
 });
